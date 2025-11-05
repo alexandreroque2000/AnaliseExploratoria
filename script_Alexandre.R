4b1e5b48ecf12b768ef6bf33cb25f020032c29a2
Alexandre Roque <alexandreroque@id.uff.br>
2025-11-05 17:01:47 -0300

    Melhorando os gráficos

--- script_Alexandre.R
+++ script_Alexandre.R
@@ -6,12 +6,14 @@ library(readxl)
 
 #Lendo a base de dados
 base_trabalho = read_excel("Base_trabalho.xlsx")
+View(base_trabalho)
 
 #Transformando as variáveis qualitativas em fatores
-base_trabalho$sexo = as.factor(base_trabalho$sexo)
-base_trabalho$filhos = as.factor(base_trabalho$filhos)
-base_trabalho$escolaridade = as.factor(base_trabalho$escolaridade)
-base_trabalho$casado = as.factor(base_trabalho$casado)
+base_trabalho$sexo = factor(base_trabalho$sexo, levels = c(0,1),labels = c("Feminino", "Masculino"))
+base_trabalho$filhos = factor(base_trabalho$filhos, levels = c(0,1), labels = c("Não","Sim"))
+base_trabalho$escolaridade = factor(base_trabalho$escolaridade, levels = c(1,2,3), labels = c("Fundamental","Médio","Superior"))
+base_trabalho$casado = factor(base_trabalho$casado, levels = c(0,1),labels = c("Não","Sim"))
+base_trabalho$reincidente = factor(base_trabalho$reincidente, levels = c(0,1),labels = c("Não","Sim"))
 
 str(base_trabalho)
 
@@ -22,21 +24,32 @@ faltantes
 
 #Fazendo um histograma da variável Idade
 ggplot(data = base_trabalho, mapping = aes(x=idade)) + 
-  geom_histogram(fill="darkgreen") + 
-  theme_bw() + xlab("Idade") + ylab("Frequência absoluta")
+  geom_histogram(breaks=seq(0,100,length.out=15),fill="darkgreen",color="black") + 
+  theme_bw() + xlab("Idade em anos") + ylab("Frequência")+
+  scale_x_continuous(breaks = seq(0,100,by=20))+
+  labs(title = "Histograma - Idade")+
+  theme(plot.title = element_text(hjust = 0.5,face = "bold",size = 14))
 
 #Fazendo um boxplot da variável tempo_preso
 ggplot(data = base_trabalho, mapping = aes(x = tempo_preso,y = " "))+
   geom_boxplot(fill = "lightblue") + 
-  theme_bw() + xlab("Tempo preso") + ylab("")
+  theme_bw() + xlab("Meses") + ylab("")+
+  labs(title = "Boxplot - Tempo preso") +
+  theme(plot.title = element_text(hjust = 0.5,face = "bold",size = 14))
 
 #Fazendo um boxplot da variável score_periculosidade por escolaridade
 ggplot(data = base_trabalho, mapping = aes(x = score_periculosidade, y = escolaridade)) +
   geom_boxplot(fill = "orange") +
-  theme_bw() + xlab("Periculosidade") + ylab("Escolaridade")
+  theme_bw() + xlab("Score de periculosidade") + ylab("Escolaridade")+
+  labs(title = "Boxplot - Periculosidade por escolaridade")+
+  theme(plot.title = element_text(hjust = 0.5,face = "bold",size = 14))
+
 
 #Fazendo um gráfico de barras para a variável reincidente
 ggplot(data = base_trabalho, mapping = aes(x = reincidente)) + 
-  geom_bar(fill = "darkred") +
-  theme_bw() + xlab("Reincidência") + ylab("")
+  geom_bar(fill = "darkred",color = "black") +
+  theme_bw() + xlab("Reincidência") + ylab("")+
+  labs(title = "Gráfico de barras - Reincidência")+
+  theme(plot.title = element_text(hjust = 0.5,face = "bold",size = 14))
+
 
